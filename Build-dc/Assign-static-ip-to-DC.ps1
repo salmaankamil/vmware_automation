e1994a449196efcb4f5656275710b91966e1d648
salmaankamil <50107950+salmaankamil@users.noreply.github.com>
2024-06-03 11:22:46 +0530

    Update Assign-static-ip-to-DC.ps1

--- Build-dc/Assign-static-ip-to-DC.ps1
+++ Build-dc/Assign-static-ip-to-DC.ps1
@@ -1,6 +1,6 @@
 # Variables
 $interfaceAlias = "Ethernet0"  # Replace with the name of your network adapter
-$ipAddress = "10.131.232.118"  # Replace with the desired static IP address
+$ipAddress = "10.131.232.119"  # Replace with the desired static IP address
 $prefixLength = 20            # Replace with the subnet prefix length (e.g., 24 for 255.255.255.0)
 $defaultGateway = "10.131.239.254"  # Replace with the desired default gateway
 $dnsServers = "10.132.7.33"  # Replace with your DNS server addresses
