15e807f18a2b09dbfd4f76cf9a66a2d8dfab7b33
salmaankamil <50107950+salmaankamil@users.noreply.github.com>
2024-06-04 18:58:47 +0530

    Update variables.ps1

--- vcenter-deploy/variables.ps1
+++ vcenter-deploy/variables.ps1
@@ -1,17 +1,17 @@
 $esxi_hostname = " "  #esx name or ip where vcenter will be deployed
 $esxi_username = "root"   #esx username and password of the esx
 $esxi_password = "VMware123!"
-$deployment_network = ""
-$datastore = ""
-$deployment_option = "small"  #tiny, small, large  etc
+$deployment_network = "GS Labs Network"
+$datastore = "datastore1"
+$deployment_option = "tiny"  #tiny, small, large  etc
 $vcenter_vm_name =  "vcenter" #vcenter VM name of the appliance
 
 
 $vcenter_fqdn_system_name = "vcenter.vsphere.local"  # ip addreess 
-$vcenter_ip = ""
+$vcenter_ip = "10.131.232.120"
 $vcenter_prefix = 20  # subnetmask
-$vcenter_gateway = ""
-$vcenter_dns_servers = ""
+$vcenter_gateway = "10.131.239.254"
+$vcenter_dns_servers = "10.131.232.119"
 
 $vcenter_ssh_password = "VMware123!"
 
