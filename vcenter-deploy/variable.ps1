3631374c1af45f9e2725c9a0301e16e58b46b142
salmaankamil <50107950+salmaankamil@users.noreply.github.com>
2024-06-04 12:19:43 +0530

    Create variable.ps1

---
+++ vcenter-deploy/variable.ps1
@@ -0,0 +1,19 @@
+$esxi_hostname = ""  #esx name or ip where vcenter will be deployed
+$esxi_username = "root"   #esx username and passowrd of the esx
+$esxi_password = "VMware123!"
+$deployment_network = ""
+$datastore = ""
+$deployment_option = ""  #tiny, small, large  etc
+$vcenter_vm_name =   #vcenter VM name of the appliance
+
+
+$vcenter_fqdn_system_name = "vcenter.vsphere.local"  # ip addreess 
+$vcenter_ip = ""
+$vcenter_prefix = 20  # subnetmask
+$vcenter_gateway = ""
+$vcenter_dns_servers = ""
+
+$vcenter_ssh_password = "VMware123!"
+
+$vcenter_sso_password = ""
+$vcenter_sso_domain_name = vsphere.local
